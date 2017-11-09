3d9384732b1809ec1a9125dd900b145ea33989b4
regston <regston@yeah.net>
2017-11-10 00:20:10 +0800

    Revert "v2"
    This reverts commit 53c623607c97588611311dcff6a2d58e873e0348.

--- pol2rect.m
+++ pol2rect.m
@@ -2,7 +2,7 @@
 % [RECT] = RECT2POL(RHO, THETA)
 % RECT - Complex matrix or number, RECT = A + jB, A = Real, B = Imaginary
 % RHO - Magnitude
-% THETA - Angle in radians .
+% THETA - Angle in radians
 
 function rect = pol2rect(rho,theta)
 rect = rho.*cos(theta) + j*rho.*sin(theta);
